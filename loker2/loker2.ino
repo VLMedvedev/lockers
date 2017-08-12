748d665ad4033b5722ae699603e56c4d79872cf3
Vladimir Medvedev <c066en@gmail.com>
2017-08-12 17:53:39 +0200

    add external wake up + reboot + 1-wire  v1

--- loker2/loker2.ino
+++ loker2/loker2.ino
@@ -1,20 +1,34 @@
-/* @file EventSerialKeypad.pde
-  || @version 1.0
-  || @author Alexander Brevig
-  || @contact alexanderbrevig@gmail.com
-  ||
-  || @description
-  || | Demonstrates using the KeypadEvent.
-  || #
-*/
+
 #include <Keypad2.h>
 #include <LiquidCrystal.h>
 #include <avr/sleep.h>
 //#include <MsTimer2.h>
-//#include <TimerOne.h>
-//#include "LowPower.h"
 #include <avr/wdt.h>
 #include <dumpmon.h>        // Include necessary header.
+#include <OneWire.h>
+
+#define DS2413_ONEWIRE_PIN  (8)
+
+#define DS2413_FAMILY_ID    0x3A
+#define DS2413_ACCESS_READ  0xF5
+#define DS2413_ACCESS_WRITE 0x5A
+#define DS2413_ACK_SUCCESS  0xAA
+#define DS2413_ACK_ERROR    0xFF
+
+OneWire oneWire(DS2413_ONEWIRE_PIN);
+uint8_t address[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+typedef struct
+{
+  byte SerialNumberUnit[9];
+  byte ID_paket[14];
+  unsigned long PassWord;
+  byte StatusUnit;
+}  record_type;
+
+record_type record[8];
+
+const byte secret[256] = "01234567890qwertyuiopasdfghjkl;zxcvbnm,./1234567890qwertyuiopasdfghjkl;zxcvbnm,./1234567890qwertyuiopasdfghjkl;zxcvbnm,./1234567890qwertyuiopasdfghjkl;zxcvbnm,./1234567890qwertyuiopasdfghjkl;zxcvbnm,./1234567890qwertyuiopasdfghjkl;zxcvbnm,./123456789012345";
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(A0, A1, A2, A3, A4, A5);
@@ -43,7 +57,9 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 const int wakeUpPin = 2;
 byte ledPin = 13;
 boolean ledPin_state;
-unsigned long timeBegin;
+String password = "      ";
+
+volatile unsigned long timeBegin;
 
 // we don't want to do anything except wake
 EMPTY_INTERRUPT (PCINT0_vect)
@@ -87,8 +103,9 @@ void setup() {
 
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("hello, world!");
+  lcd.print("Put password !");
   lcd.setCursor(0, 1);
+  lcd.print(password);
 
   // pin change interrupt masks (see above list)
   PCMSK2 |= _BV (PCINT21);   // pin 5  - PORTD mask 0x20
@@ -96,19 +113,24 @@ void setup() {
   PCMSK2 |= _BV (PCINT23);   // pin 7  - PORTD mask 0x80
   PCMSK0 |= _BV (PCINT0);    // pin 8  - PORTB mask 0x01
 
-   // Configure wake up pin as input.
-    // This will consumes few uA of current.
-    pinMode(wakeUpPin, INPUT_PULLUP);  
-   // Allow wake up pin to trigger interrupt on low.
-    attachInterrupt(0, wakeUp, LOW);
+  // Configure wake up pin as input.
+  // This will consumes few uA of current.
+  pinMode(wakeUpPin, INPUT_PULLUP);
+  // Allow wake up pin to trigger interrupt on low.
+  attachInterrupt(0, wakeUp, LOW);
 
-    
+
+
   timeBegin = millis();
   //prints time since program started
   //Serial.println(timeBegin);
- 
+
   // setup_watchdog(7);
   //dumpmonSetup(115200);
+
+  test_1_wire();
+ 
+  get_box_table ();
 
 }
 
@@ -187,8 +209,8 @@ void reconfigurePins ()
 void wakeUp()
 {
   timeBegin = millis();
-   Serial.println ("external wake up ...");
-  // delay(50);
+  Serial.println ("external wake up ...");
+
   reboot();
 }
 
@@ -225,19 +247,13 @@ void goToSleep ()
 
 
   Serial.println ("Going to sleep ...");
-  /*
-    Serial.print ("PINB = ");
-    Serial.println (PINB, HEX);
-    Serial.print ("PIND = ");
-    Serial.println (PIND, HEX);
-  */
 
-   // Configure wake up pin as input.
-    // This will consumes few uA of current.
-    pinMode(wakeUpPin, INPUT_PULLUP);  
-   // Allow wake up pin to trigger interrupt on low.
-    attachInterrupt(0, wakeUp, LOW);
-  
+  // Configure wake up pin as input.
+  // This will consumes few uA of current.
+  pinMode(wakeUpPin, INPUT_PULLUP);
+  // Allow wake up pin to trigger interrupt on low.
+  attachInterrupt(0, wakeUp, LOW);
+
   // overcome any debounce delays built into the keypad library
   delay (50);
 
@@ -270,12 +286,7 @@ void goToSleep ()
 
 
   Serial.println ("Awake!");
-  /*
-    Serial.print ("PINB = ");
-    Serial.println (PINB, HEX);
-    Serial.print ("PIND = ");
-    Serial.println (PIND, HEX);
-  */
+
   // put keypad pins back how they are expected to be
   reconfigurePins ();
   digitalWrite(ledPin, HIGH);
@@ -308,7 +319,7 @@ void loop() {
 
   if (key) {
     Serial.println(key);
-    lcd.print((char) key);
+    set_password(key);
     timeBegin = millis();
   }
 
@@ -351,3 +362,155 @@ void keypadEvent(KeypadEvent key) {
       break;
   }
 }
+
+/********************************************************/
+
+
+unsigned long calk_password (byte ID_box) 
+{
+  
+}
+
+void get_box_table ()
+{
+  record[0].SerialNumberUnit[9] = "A1234567";
+  record[0].ID_paket[14] = "RB298973247SG";
+  record[0].PassWord = calk_password (0) ;
+  record[0].StatusUnit = 0x43;
+  
+  record[1] = (record_type) { "3A234567", "LF202929536CN", 123456, 0x43 };
+  record[2] = (record_type) { "3A234568", "RB298973247SG", 654321, 0x43 };
+}
+
+void set_password(char key)
+{
+  for (int i = 0; i <= 5; i++)
+  {
+    password.setCharAt(i, password.charAt(i + 1));
+  }
+  password.setCharAt(5, key);
+  lcd.setCursor(0, 1);
+  //  lcd.print((char) key);
+  lcd.print(password);
+
+}
+
+/***************************************1-Wire***************************/
+
+void printBytes(uint8_t* addr, uint8_t count, bool newline=0) 
+{
+  for (uint8_t i = 0; i < count; i++) 
+  {
+    Serial.print(addr[i]>>4, HEX);
+    Serial.print(addr[i]&0x0f, HEX);
+    Serial.print(" ");
+  }
+  if (newline)
+  {
+    Serial.println();
+  }
+}
+
+byte read(void)
+{    
+  bool ok = false;
+  uint8_t results;
+
+  oneWire.reset();
+  oneWire.select(address);
+  oneWire.write(DS2413_ACCESS_READ);
+
+  results = oneWire.read();                 /* Get the register results   */
+  ok = (!results & 0x0F) == (results >> 4); /* Compare nibbles            */
+  results &= 0x0F;                          /* Clear inverted values      */
+
+  oneWire.reset();
+  
+  // return ok ? results : -1;
+  return results;
+}
+
+bool write(uint8_t state)
+{
+  uint8_t ack = 0;
+  
+  /* Top six bits must '1' */
+  state |= 0xFC;
+  
+  oneWire.reset();
+  oneWire.select(address);
+  oneWire.write(DS2413_ACCESS_WRITE);
+  oneWire.write(state);
+  oneWire.write(~state);                    /* Invert data and resend     */    
+  ack = oneWire.read();                     /* 0xAA=success, 0xFF=failure */  
+  if (ack == DS2413_ACK_SUCCESS)
+  {
+    oneWire.read();                          /* Read the status byte      */
+  }
+  oneWire.reset();
+    
+  return (ack == DS2413_ACK_SUCCESS ? true : false);
+}
+
+void test_1_wire()
+{
+   
+  Serial.println(F("Looking for a DS2413 on the bus"));
+  
+  /* Try to find a device on the bus */
+  oneWire.reset_search();
+  delay(250);
+  if (!oneWire.search(address)) 
+  {
+    printBytes(address, 8);
+    Serial.println(F("No device found on the bus!"));
+    oneWire.reset_search();
+    while(1);
+  }
+  
+  /* Check the CRC in the device address */
+  if (OneWire::crc8(address, 7) != address[7]) 
+  {
+    Serial.println(F("Invalid CRC!"));
+    while(1);
+  }
+  
+  /* Make sure we have a DS2413 */
+  if (address[0] != DS2413_FAMILY_ID) 
+  {
+    printBytes(address, 8);
+    Serial.println(F(" is not a DS2413!"));
+    while(1);
+  }
+  
+  Serial.print(F("Found a DS2413: "));
+  printBytes(address, 8);
+  Serial.println(F(""));
+
+  test_wire1() ;
+}
+
+void test_wire1() 
+{
+  /* Read */
+  
+  uint8_t state = read();
+  if (state == -1)
+    Serial.println(F("Failed reading the DS2413"));
+  else
+    Serial.println(state, BIN);
+
+  Serial.println();
+    
+  /* Write */
+  /*
+  bool ok = false;
+  ok = write(0x3);
+  if (!ok) Serial.println(F("Wire failed"));
+  delay(1000);
+  ok = write(0x0);
+  if (!ok) Serial.println(F("Wire failed"));
+  delay(1000);
+ */
+}
+
